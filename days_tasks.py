e78ecaaa1f944176994d568f998b10d039c54906
Ruslan Dovhanych <imdakris@gmail.com>
2023-04-26 11:45:11 +0300

    finish days_tasks

--- days_tasks.py
+++ days_tasks.py
@@ -1,6 +1,6 @@
+#!/bin/python3
 import schedule
 import requests
-
 
 def greeting():
     todos_dict = {
@@ -15,11 +15,18 @@ def greeting():
         
     response = requests.get(url='https://yobit.net/api/3/ticker/btc_usd')
     data = response.json()
-    btc_price = f"BTC: {round(data.get('btc_usd').get('last'), 2)}$"
+    btc_price = f"BTC: {round(data.get('btc_usd').get('last'), 2)}$\n"
     print(btc_price)
 
 def main():
-    greeting()
-
+    # greeting()
+    schedule.every().day.at('11:44').do(greeting)
+    schedule.every().day.thursday.do(greeting)
+    schedule.every().friday.at('11:41').do(greeting)
+    
+    while True:
+        schedule.run_pending()
+        
+        
 if __name__ == '__main__':
     main()
